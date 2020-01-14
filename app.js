a555f00c3871a0f10903b799e87678b9b9483680
Liviu Buciuman <lbuciuman@hotmail.com>
2020-01-14 16:48:34 +0100

    Last commit

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const arr = [1, 3, 5, 7, 9];
-gradingStudents(arr);
+const grades = [4, 73, 67, 38, 33];
+console.log(gradingStudents(grades));
 
 function gradingStudents(grades) {
-    // Write
-    // your code here
-
+    return grades.map(function (grade) {
+        if (grade >= 38 && grade % 5 >= 3) {
+            return grade + 5 - (grade % 5);
+        } else {
+            return grade;
+        }
+    });
 }
